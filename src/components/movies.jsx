c061824dcd2a1d74f84a3e2c634c93c754f50ab1
Vedant <kalpved123@gmail.com>
2022-02-11 05:22:30 +0530

    conditional rendering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,37 +14,42 @@ class Movies extends Component {
 this.setState({movies}); // to update the state!!
  };
      
-    
+ 
+ render() {  
+      const {length:count }= this.state.movies; //picking len proprty of movies , obj destructuring
 
-    render() {  
+      if(count===0) return <p >There are no movies in the database</p>;
       
- return <table className='table'>
-  <thead>
-    <tr>
-      <th>Title</th>
-      <th>Genre</th>
-      <th>Stock</th>
-      <th>Rate</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    {this.state.movies.map(movie=>(
-    <tr key={movie._id}>
-      <td>{movie.title}</td>
-      <td>{movie.genre.name}</td>
-      <td>{movie.numberInStock}</td>
-      <td>{movie.dailyRentalRate}</td>
-      <td><button onClick={()=>(this.handleDelete(movie))} className="btn btn-danger btn-sm">Delete</button></td>
-      {/* in order to pass an arg we need to change it to arrow function jo map wala movie h wAHI WALA H YE BHI */}
-    </tr>
-    ))}
-    
-  </tbody>
-</table>
-      
-    }
+ return(
+ <>
+ <p>Showing {count}  movies in the database</p>
+   <table className='table'>
+       <thead>
+         <tr>
+           <th>Title</th>
+           <th>Genre</th>
+           <th>Stock</th>
+           <th>Rate</th>
+           <th></th>
+         </tr>
+       </thead>
+       <tbody>
+         {this.state.movies.map(movie => (
+           <tr key={movie._id}>
+             <td>{movie.title}</td>
+             <td>{movie.genre.name}</td>
+             <td>{movie.numberInStock}</td>
+             <td>{movie.dailyRentalRate}</td>
+             <td><button onClick={() => (this.handleDelete(movie))} className="btn btn-danger btn-sm">Delete</button></td>
+             {/* in order to pass an arg we need to change it to arrow function jo map wala movie h wAHI WALA H YE BHI */}
+           </tr>
+         ))}
+       </tbody>
+     </table></> 
   
+   );
+  }
 }
+  
  
 export default Movies;

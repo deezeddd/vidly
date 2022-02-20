615b0cc68065981cffc1b69ebb010eee19bfcdd0
Vedant <kalpved123@gmail.com>
2022-02-20 13:28:32 +0530

    pagination component interface

--- src/components/mov.jsx
+++ src/components/mov.jsx
@@ -1,49 +1,55 @@
-import React, { Component } from 'react';
-import {getMovies} from '../services/fakeMovieService'
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Page from "./common/pagination";
 class Movies extends Component {
-    state = { movies:getMovies(),
-                           
-    } ;
-    handleDelete=(movie)=>{
-// console.log('egsfd');
-const movies=this.state.movies.filter(m=>m._id!==movie._id)
-this.setState({movies})
-    }
-    render() { 
-        const count= this.state.movies.length
-        // if(count===0) return <p>fuck off</p>
-        
-        return (
-            <>
-            <p>No of movies left {count}</p>
-            
-                <table className='table'>
-                <thead>
-                    <tr>
-                        <th>title</th>
-                        <th>genre</th>
-                        <th>no in stock</th>
-                        <th>rating</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody >
-                {this.state.movies.map(c=>
-                    <tr key={c._id}>
-                        <td>{c.title}</td>
-                        
-                        <td>{c.genre.name}</td>
-                        <td>{c.numberInStock}</td>
-                        <td>{c.dailyRentalRate}</td>
-                        <td><button onClick={()=>this.handleDelete(c)} className="btn btn-danger btn-small m-2">Delete</button></td>
-                    </tr>
-                )}
-                </tbody>
-            </table>
-            </>
-        )
-        
-    }
+  state = { movies: getMovies() };
+  handleDelete = (movie) => {
+    // console.log('egsfd');
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  render() {
+    const count = this.state.movies.length;
+    // if(count===0) return <p>fuck off</p>
+
+    return (
+      <>
+        <p>No of movies left {count}</p>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>title</th>
+              <th>genre</th>
+              <th>no in stock</th>
+              <th>rating</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((c) => (
+              <tr key={c._id}>
+                <td>{c.title}</td>
+
+                <td>{c.genre.name}</td>
+                <td>{c.numberInStock}</td>
+                <td>{c.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(c)}
+                    className="btn btn-danger btn-small m-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <Page />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
- 
+
 export default Movies;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,65 +1,86 @@
 // import react from 'react';
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from './common/Liked'
+import Like from "./common/Liked";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
-    state = { 
-        movies :getMovies(),
-
-        
-     } ;
- handleDelete=(movie)=>{ 
-  const movies= this.state.movies.filter(m=>m._id !== movie._id); //new movies array which includes all movies except current movie obj
-this.setState({movies}); 
- };
-
-  handleLike=(movie)=>{
-    // console.log('like clicked',movie);
-    const movies=[...this.state.movies]
-    const index=movies.indexOf(movie)
-    movies[index]={...movies[index]}
-    movies[index].liked=!movies[index].liked;
-    this.setState({movies});
+  state = {
+    movies: getMovies(),
+    pageSize: 4,
   };
- 
- render() {  
-      const {length:count }= this.state.movies; //picking len proprty of movies , obj destructuring
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id); //new movies array which includes all movies except current movie obj
+    this.setState({ movies });
+  };
 
-      if(count===0) return <p >There are no movies in the database</p>;
-      
- return(
- <>
- <p>Showing {count}  movies in the database</p>
-   <table className='table'>
-       <thead>
-         <tr>
-           <th>Title</th>
-           <th>Genre</th>
-           <th>Stock</th>
-           <th>Rate</th>
-           <th></th>
-           <th></th>
-         </tr>
-       </thead>
-       <tbody>
-         {this.state.movies.map(movie => (
-           <tr key={movie._id}>
-             <td>{movie.title}</td>
-             <td>{movie.genre.name}</td>
-             <td>{movie.numberInStock}</td>
-             <td>{movie.dailyRentalRate}</td>
-            <td>  <Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/>  </td> 
-             <td><button onClick={() => (this.handleDelete(movie))} className="btn btn-danger btn-sm">Delete</button></td>
-             {/* in order to pass an arg we need to change it to arrow function jo map wala movie h wAHI WALA H YE BHI */}
-           </tr>
-         ))}
-       </tbody>
-     </table></> 
-  
-   );
+  handleLike = (movie) => {
+    // console.log('like clicked',movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+  handlePageChange = (page) => {
+    console.log(page);
+  };
+
+  render() {
+    const { length: count } = this.state.movies; //picking len proprty of movies , obj destructuring
+
+    if (count === 0) return <p>There are no movies in the database</p>;
+
+    return (
+      <>
+        <p>Showing {count} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  {" "}
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />{" "}
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+                {/* in order to pass an arg we need to change it to arrow function jo map wala movie h wAHI WALA H YE BHI */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
+        {/* at a minimum we need to give this comp total no of items as well as pagesize*/}
+      </>
+    );
   }
 }
-  
- 
+
 export default Movies;

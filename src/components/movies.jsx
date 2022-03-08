193cff34c6113cb5f5db9c817e52a80fdbccc111
Vedant <kalpved123@gmail.com>
2022-03-08 07:03:13 +0530

    paginating data

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,7 @@
 // import react from 'react';
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
 import Like from "./common/Liked";
 import Pagination from "./common/pagination";
 
@@ -29,9 +30,9 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies; //picking len proprty of movies , obj destructuring
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
     if (count === 0) return <p>There are no movies in the database</p>;
-
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <>
         <p>Showing {count} movies in the database</p>
@@ -47,7 +48,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
